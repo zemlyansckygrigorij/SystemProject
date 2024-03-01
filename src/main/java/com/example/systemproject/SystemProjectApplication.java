27cb7449371976d274549916b1f0a5ea84de0e0f
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2024-03-01 12:52:50 +0500

    Initial commit

---
+++ src/main/java/com/example/systemproject/SystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/systemproject/SystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
